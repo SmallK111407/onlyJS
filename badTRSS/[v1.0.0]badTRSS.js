f795e791af8ff80bb3e74c62d55b0d6083af6b5e
SmallK111407 <3399280843@qq.com>
2023-08-07 15:01:51 +0800

    新增 badTRSS.js

---
+++ badTRSS/[v1.0.0]badTRSS.js
@@ -0,0 +1,40 @@
+import plugin from '../../lib/plugins/plugin.js';
+
+/*
+更新日志
+v1.0.0 该插件通过“抢优先级”的方式防止时雨偷家
+*/
+
+//想要允许时雨使用哪些命令呢？ 
+//包含allowCommand里的就会尝试执行其他符合正则的方式
+const allowCommand = ['关机', '重启', '状态', '撤回', '修改头衔', '申请头衔']
+
+export class badTRSS extends plugin {
+    constructor() {
+        super({
+            name: '啊啊啊时雨你真是太坏了，我要生气了',
+            dsc: '时雨好坏！',
+            event: 'message',
+            priority: -100000000,
+            rule: [
+                {
+                    reg: '.*',
+                    fnc: 'badTRSS',
+                    log: false
+                }
+            ]
+        })
+    }
+
+    async badTRSS() {
+        if (this.e.user_id == 2536554304) {
+            if (allowCommand.includes(this.e.msg)) {
+                return false
+            } else {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
+}
